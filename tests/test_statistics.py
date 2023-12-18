d3fb1086a9ef266b4db3ec58f8edb598551d14a6
nilsnevertree <49751662+nilsnevertree@users.noreply.github.com>
2023-12-18 12:35:48 +0100

    linting tests

--- tests/test_statistics.py
+++ tests/test_statistics.py
@@ -1,19 +1,17 @@
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
+import pytest
+import xarray as xr
+
 from scipy import signal
 from sdm_eurec4a import statistics
-import xarray as xr
-import pytest
 
 
 def test_RMSE():
     # Example 1: Test with arrays of equal length
     x = np.array([1, 2, 3])
     y = np.array([2, 4, 6])
-    np.testing.assert_equal(
-        statistics.RMSE(x, y),
-        np.sqrt(14/3)
-    )
+    np.testing.assert_equal(statistics.RMSE(x, y), np.sqrt(14 / 3))
 
     # Example 2: Test with arrays of different length
     with pytest.raises(ValueError):
