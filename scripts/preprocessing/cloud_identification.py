d62524e945ce39c146b451de170aee3d7444ec1a
nilsnevertree <49751662+nilsnevertree@users.noreply.github.com>
2024-01-12 09:44:37 +0100

    use main function

--- scripts/preprocessing/cloud_identification.py
+++ scripts/preprocessing/cloud_identification.py
@@ -123,115 +123,120 @@ logging.info("Destination filename: %s", OUTPUT_FILE_NAME)
 logging.info("Mask name: %s", mask_name)
 
 
-cloud_composite = xr.open_dataset(INPUT_FILEPATH, chunks={"time": 1000})
+def main():
+    cloud_composite = xr.open_dataset(INPUT_FILEPATH, chunks={"time": 1000})
 
-with ProgressBar():
-    logging.info("Identify clouds using xr.diff")
-    cloud_diff = cloud_composite[mask_name].fillna(0).astype(int).diff(dim="time")
-    cloud_diff = cloud_diff.compute()
-    cloud_start = cloud_diff.time.where(cloud_diff == 1, drop=True)
-    cloud_end = cloud_diff.time.where(cloud_diff == -1, drop=True)
-    logging.info(f"{cloud_start.shape} number of clouds were identified")
+    with ProgressBar():
+        logging.info("Identify clouds using xr.diff")
+        cloud_diff = cloud_composite[mask_name].fillna(0).astype(int).diff(dim="time")
+        cloud_diff = cloud_diff.compute()
+        cloud_start = cloud_diff.time.where(cloud_diff == 1, drop=True)
+        cloud_end = cloud_diff.time.where(cloud_diff == -1, drop=True)
+        logging.info(f"{cloud_start.shape} number of clouds were identified")
 
-    logging.info("Create cloud identification dataset")
-    clouds = xr.Dataset(
-        coords={"cloud_id": np.arange(0, cloud_start.size)},
+        logging.info("Create cloud identification dataset")
+        clouds = xr.Dataset(
+            coords={"cloud_id": np.arange(0, cloud_start.size)},
+        )
+        details = (
+            f"Cloud identification is done by using the {mask_name} from the original dataset.\n"
+            f"All np.nan values in the {mask_name} are set to 0/False.\n"
+            f"The original {mask_name} is then used to identify individual clouds.\n"
+        )
+
+        clouds.attrs = {
+            "description": "cloud identification dataset",
+            "creation_time": str(datetime.datetime.now()),
+            "details": details,
+            "author": "Nils Niebaum",
+            "email": "nils-ole.niebaum@mpimet.mpg.de",
+            "institution": "Max Planck Institute for Meteorology",
+        }
+
+        clouds["start"] = ("cloud_id", cloud_start.data)
+        clouds["start"].attrs = {"long_name": "start time of cloud event"}
+        clouds["end"] = ("cloud_id", cloud_end.data)
+        clouds["end"].attrs = {"long_name": "end time of cloud event"}
+        clouds["duration"] = clouds.end - clouds.start
+        clouds["duration"].attrs = {"long_name": "duration of cloud event"}
+        clouds["mid_time"] = clouds.start + clouds.duration / 2
+        clouds["mid_time"].attrs = {"long_name": "mid time of cloud event"}
+        # # The following code is omitted because storing objects is not a good idea.
+        # clouds['selection'] = ('cloud_id', [(start, end) for start, end in zip(clouds.start.data, clouds.end.data)])
+        # clouds['selection'].attrs = {
+        #         'long_name': 'time selection of cloud event',
+        #         'description': 'tupel of (start, end) for the cloud event. This can help to select the cloud event from the original dataset'
+        #         }
+
+        # Define
+        clouds = clouds.assign_coords({"time": clouds.mid_time})
+        clouds = clouds.swap_dims({"cloud_id": "time"})
+        logging.info("Store cloud identification dataset")
+        clouds.to_netcdf(OUTPUT_DIR / "temporary.nc")
+
+    clouds = xr.open_dataset(OUTPUT_DIR / "temporary.nc")
+
+    logging.info("Calculate total LWC of cloud events")
+    clouds["liquid_water_content"] = (
+        "time",
+        [
+            cloud_composite["liquid_water_content"].sel(time=slice(start, end)).sum()
+            for start, end in zip(clouds.start.data, clouds.end.data)
+        ],
     )
-    details = (
-        f"Cloud identification is done by using the {mask_name} from the original dataset.\n"
-        f"All np.nan values in the {mask_name} are set to 0/False.\n"
-        f"The original {mask_name} is then used to identify individual clouds.\n"
-    )
-
-    clouds.attrs = {
-        "description": "cloud identification dataset",
-        "creation_time": str(datetime.datetime.now()),
-        "details": details,
-        "author": "Nils Niebaum",
-        "email": "nils-ole.niebaum@mpimet.mpg.de",
-        "institution": "Max Planck Institute for Meteorology",
+    clouds["liquid_water_content"].attrs = {
+        "long_name": "total LWC of cloud event",
+        "units": "g/m3",
+        "comment": "This is the sum of the LWC of all pixels in the cloud event.\nMass of all droplets per cubic meter of air, assuming water spheres with density = 1g/cm3",
     }
 
-    clouds["start"] = ("cloud_id", cloud_start.data)
-    clouds["start"].attrs = {"long_name": "start time of cloud event"}
-    clouds["end"] = ("cloud_id", cloud_end.data)
-    clouds["end"].attrs = {"long_name": "end time of cloud event"}
-    clouds["duration"] = clouds.end - clouds.start
-    clouds["duration"].attrs = {"long_name": "duration of cloud event"}
-    clouds["mid_time"] = clouds.start + clouds.duration / 2
-    clouds["mid_time"].attrs = {"long_name": "mid time of cloud event"}
-    # # The following code is omitted because storing objects is not a good idea.
-    # clouds['selection'] = ('cloud_id', [(start, end) for start, end in zip(clouds.start.data, clouds.end.data)])
-    # clouds['selection'].attrs = {
-    #         'long_name': 'time selection of cloud event',
-    #         'description': 'tupel of (start, end) for the cloud event. This can help to select the cloud event from the original dataset'
-    #         }
+    logging.info("Calculate mean altitude of cloud events")
+    clouds["alt"] = (
+        "time",
+        [
+            cloud_composite["alt"].sel(time=slice(start, end)).mean()
+            for start, end in zip(clouds.start.data, clouds.end.data)
+        ],
+    )
+    clouds["alt"].attrs = {
+        "long_name": "mean altitude of cloud event",
+        "units": "m",
+        "comment": "This is the mean altitude of all pixels in the cloud event.\nFrom SAFIRE ATR42 Inertial/GPS System",
+    }
 
-    # Define
-    clouds = clouds.assign_coords({"time": clouds.mid_time})
-    clouds = clouds.swap_dims({"cloud_id": "time"})
-    logging.info("Store cloud identification dataset")
-    clouds.to_netcdf(OUTPUT_DIR / "temporary.nc")
+    logging.info("Calculate mean latitude of cloud events")
+    clouds["lat"] = (
+        "time",
+        [
+            cloud_composite["lat"].sel(time=slice(start, end)).mean()
+            for start, end in zip(clouds.start.data, clouds.end.data)
+        ],
+    )
+    clouds["lat"].attrs = {
+        "long_name": "mean latitude of cloud event",
+        "units": "degree",
+        "comment": "This is the mean latitude of all pixels in the cloud event.\nFrom SAFIRE ATR42 Inertial/GPS System.",
+    }
 
-clouds = xr.open_dataset(OUTPUT_DIR / "temporary.nc")
+    logging.info("Calculate mean longitude of cloud events")
+    clouds["lon"] = (
+        "time",
+        [
+            cloud_composite["lon"].sel(time=slice(start, end)).mean()
+            for start, end in zip(clouds.start.data, clouds.end.data)
+        ],
+    )
+    clouds["lon"].attrs = {
+        "long_name": "mean longitude of cloud event",
+        "units": "degree",
+        "comment": "This is the mean longitude of all pixels in the cloud event.\nFrom SAFIRE ATR42 Inertial/GPS System.",
+    }
 
-logging.info("Calculate total LWC of cloud events")
-clouds["liquid_water_content"] = (
-    "time",
-    [
-        cloud_composite["liquid_water_content"].sel(time=slice(start, end)).sum()
-        for start, end in zip(clouds.start.data, clouds.end.data)
-    ],
-)
-clouds["liquid_water_content"].attrs = {
-    "long_name": "total LWC of cloud event",
-    "units": "g/m3",
-    "comment": "This is the sum of the LWC of all pixels in the cloud event.\nMass of all droplets per cubic meter of air, assuming water spheres with density = 1g/cm3",
-}
+    with ProgressBar():
+        clouds.to_netcdf(OUTPUT_DIR / OUTPUT_FILE_NAME)
+    logging.info("Successfully finished cloud identification pre-processing")
+    logging.info(f"File written to {OUTPUT_DIR / OUTPUT_FILE_NAME}")
 
-logging.info("Calculate mean altitude of cloud events")
-clouds["alt"] = (
-    "time",
-    [
-        cloud_composite["alt"].sel(time=slice(start, end)).mean()
-        for start, end in zip(clouds.start.data, clouds.end.data)
-    ],
-)
-clouds["alt"].attrs = {
-    "long_name": "mean altitude of cloud event",
-    "units": "m",
-    "comment": "This is the mean altitude of all pixels in the cloud event.\nFrom SAFIRE ATR42 Inertial/GPS System",
-}
 
-logging.info("Calculate mean latitude of cloud events")
-clouds["lat"] = (
-    "time",
-    [
-        cloud_composite["lat"].sel(time=slice(start, end)).mean()
-        for start, end in zip(clouds.start.data, clouds.end.data)
-    ],
-)
-clouds["lat"].attrs = {
-    "long_name": "mean latitude of cloud event",
-    "units": "degree",
-    "comment": "This is the mean latitude of all pixels in the cloud event.\nFrom SAFIRE ATR42 Inertial/GPS System.",
-}
-
-logging.info("Calculate mean longitude of cloud events")
-clouds["lon"] = (
-    "time",
-    [
-        cloud_composite["lon"].sel(time=slice(start, end)).mean()
-        for start, end in zip(clouds.start.data, clouds.end.data)
-    ],
-)
-clouds["lon"].attrs = {
-    "long_name": "mean longitude of cloud event",
-    "units": "degree",
-    "comment": "This is the mean longitude of all pixels in the cloud event.\nFrom SAFIRE ATR42 Inertial/GPS System.",
-}
-
-with ProgressBar():
-    clouds.to_netcdf(OUTPUT_DIR / OUTPUT_FILE_NAME)
-logging.info("Successfully finished cloud identification pre-processing")
-logging.info(f"File written to {OUTPUT_DIR / OUTPUT_FILE_NAME}")
+if __name__ == "__main__":
+    main()
