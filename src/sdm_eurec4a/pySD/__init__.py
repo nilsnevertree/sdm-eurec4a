0c81bfec64d318a4eb723f6612be709f49556e0b
nilsnevertree <49751662+nilsnevertree@users.noreply.github.com>
2024-01-12 09:53:08 +0100

    linting

--- src/sdm_eurec4a/pySD/__init__.py
+++ src/sdm_eurec4a/pySD/__init__.py
@@ -1,2 +1,6 @@
-"""A lightweight copy of the pySDM package. From https://github.com/yoctoyotta1024/CLEO"""
+"""
+A lightweight copy of the pySDM package.
+
+From https://github.com/yoctoyotta1024/CLEO
+"""
 from __future__ import annotations
