685b3f71f442cb07c248bec7ff5c82f104ed7999
nilsnevertree <49751662+nilsnevertree@users.noreply.github.com>
2024-01-12 09:43:32 +0100

    include logger and user specified mask name (#22)

--- scripts/preprocessing/cloud_identification.py
+++ scripts/preprocessing/cloud_identification.py
@@ -1,6 +1,8 @@
 """
-This script identifies clouds in the ``cloud_mask`` of the cloud composite dataset.
-All np.nan values in the cloud_mask are set to 0/False.
+This script identifies clouds in the cloud composite dataset.
+It uses one of the masks from the original dataset to identify clouds.
+This mask can be selected by setting the variable ``mask_name``.
+All np.nan values in the rain_mask are set to 0/False.
 
 A new dataset is created which looks like this:
 <xarray.Dataset>
@@ -40,7 +42,9 @@ github_username: nilsnevertree
 # %%
 
 import datetime
+import logging
 import os
+import sys
 
 from pathlib import Path
 
@@ -53,43 +57,90 @@ from dask.diagnostics import ProgressBar
 # %%
 # Example dataset
 script_path = os.path.abspath(__file__)
-print(script_path)
+print(f"Script path is\n\t{script_path}")
 
-REPOSITORY_ROOT = Path(script_path).parent.parent.parent
-print(REPOSITORY_ROOT)
+REPO_PATH = Path(script_path).parent.parent.parent
+print(f"Repository root is\n\t{REPO_PATH}")
 
-output_path = REPOSITORY_ROOT / Path("data/observation/cloud_composite/processed/")
-output_path.mkdir(parents=True, exist_ok=True)
+OUTPUT_DIR = REPO_PATH / Path("data/observation/cloud_composite/processed/identified_clouds/")
+OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
+print(f"Output directory is\n\t{OUTPUT_DIR}")
 
-FILEPATH = REPOSITORY_ROOT / Path("data/observation/cloud_composite/processed/cloud_composite.nc")
-print(FILEPATH)
-cloud_composite = xr.open_dataset(FILEPATH, chunks={"time": 1000})
-cloud_composite = cloud_composite
+INPUT_FILEPATH = REPO_PATH / Path("data/observation/cloud_composite/processed/cloud_composite.nc")
+print(f"Input file path is\n\t{INPUT_FILEPATH}")
 
-# min_duration_new = 5
-# cm_new = cm_org.fillna(0).astype(bool)
-# cm_new = consecutive_events_xr(cm_new, min_duration = min_duration_new, axis="time")
-# with ProgressBar():
-#         cm_new = cm_new.compute()
-input("Press Enter to continue...")
+# specify the mask to use for cloud identification
+mask_name = "cloud_mask"
+print(f"Use mask '{mask_name}' to identify clouds")
+
+OUTPUT_FILE_NAME = f"identified_clouds_{mask_name}.nc"
+print(f"Output file name is\n\t'{OUTPUT_FILE_NAME}'")
+
+
+# prepare logger
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+# create a file handler
+handler = logging.FileHandler(OUTPUT_DIR / "cloud_identification.log")
+handler.setLevel(logging.INFO)
+
+# create a console handler
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+
+# create a logging format
+formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+handler.setFormatter(formatter)
+console_handler.setFormatter(formatter)
+
+# add the handlers to the logger
+logger.addHandler(handler)
+logger.addHandler(console_handler)
+
+
+def handle_exception(exc_type, exc_value, exc_traceback):
+    if issubclass(exc_type, KeyboardInterrupt):
+        sys.__excepthook__(exc_type, exc_value, exc_traceback)
+        return
+
+    logger.critical(
+        "Execution terminated due to an Exception", exc_info=(exc_type, exc_value, exc_traceback)
+    )
+
+
+sys.excepthook = handle_exception
+
+
 # %%
+
+logging.info("============================================================")
+logging.info("Start cloud identification pre-processing")
+logging.info("Input file: %s", INPUT_FILEPATH.relative_to(REPO_PATH))
+logging.info("Destination directory: %s", OUTPUT_DIR.relative_to(REPO_PATH))
+logging.info("Destination filename: %s", OUTPUT_FILE_NAME)
+logging.info("Mask name: %s", mask_name)
+
+
+cloud_composite = xr.open_dataset(INPUT_FILEPATH, chunks={"time": 1000})
+
 with ProgressBar():
-    cloud_diff = cloud_composite.cloud_mask.fillna(0).astype(int).diff(dim="time")
+    logging.info("Identify clouds using xr.diff")
+    cloud_diff = cloud_composite[mask_name].fillna(0).astype(int).diff(dim="time")
     cloud_diff = cloud_diff.compute()
     cloud_start = cloud_diff.time.where(cloud_diff == 1, drop=True)
     cloud_end = cloud_diff.time.where(cloud_diff == -1, drop=True)
-    
-    print(f"{cloud_start.shape} number of clouds were identified")
+    logging.info(f"{cloud_start.shape} number of clouds were identified")
 
-    # %%
-    print("Create cloud identification dataset")
+    logging.info("Create cloud identification dataset")
     clouds = xr.Dataset(
         coords={"cloud_id": np.arange(0, cloud_start.size)},
     )
     details = (
-        "cloud identification is done by using the cloud_mask from the original dataset.\n"
-        "All np.nan values in the cloud_mask are set to 0/False.\n"
-        "The original cloud_mask is then used to identify individual clouds.\n"
+        f"Cloud identification is done by using the {mask_name} from the original dataset.\n"
+        f"All np.nan values in the {mask_name} are set to 0/False.\n"
+        f"The original {mask_name} is then used to identify individual clouds.\n"
     )
 
     clouds.attrs = {
@@ -119,13 +170,12 @@ with ProgressBar():
     # Define
     clouds = clouds.assign_coords({"time": clouds.mid_time})
     clouds = clouds.swap_dims({"cloud_id": "time"})
-    print("Store cloud identification dataset.")
-    clouds.to_netcdf(output_path / Path("identified_clouds.nc"))
+    logging.info("Store cloud identification dataset")
+    clouds.to_netcdf(OUTPUT_DIR / "temporary.nc")
 
-# %%
-clouds = xr.open_dataset(output_path / Path("identified_clouds.nc"))
+clouds = xr.open_dataset(OUTPUT_DIR / "temporary.nc")
 
-print("Calculate total LWC of cloud events.")
+logging.info("Calculate total LWC of cloud events")
 clouds["liquid_water_content"] = (
     "time",
     [
@@ -139,7 +189,7 @@ clouds["liquid_water_content"].attrs = {
     "comment": "This is the sum of the LWC of all pixels in the cloud event.\nMass of all droplets per cubic meter of air, assuming water spheres with density = 1g/cm3",
 }
 
-print("Calculate mean altitude of cloud events.")
+logging.info("Calculate mean altitude of cloud events")
 clouds["alt"] = (
     "time",
     [
@@ -153,7 +203,7 @@ clouds["alt"].attrs = {
     "comment": "This is the mean altitude of all pixels in the cloud event.\nFrom SAFIRE ATR42 Inertial/GPS System",
 }
 
-print("Calculate mean latitude of cloud events.")
+logging.info("Calculate mean latitude of cloud events")
 clouds["lat"] = (
     "time",
     [
@@ -167,7 +217,7 @@ clouds["lat"].attrs = {
     "comment": "This is the mean latitude of all pixels in the cloud event.\nFrom SAFIRE ATR42 Inertial/GPS System.",
 }
 
-print("Calculate mean longitude of cloud events.")
+logging.info("Calculate mean longitude of cloud events")
 clouds["lon"] = (
     "time",
     [
@@ -181,8 +231,7 @@ clouds["lon"].attrs = {
     "comment": "This is the mean longitude of all pixels in the cloud event.\nFrom SAFIRE ATR42 Inertial/GPS System.",
 }
 
-print("Save new dataset to disk.")
 with ProgressBar():
-    clouds.to_netcdf(output_path / Path("identified_clouds_more.nc"))
-
-# %%
+    clouds.to_netcdf(OUTPUT_DIR / OUTPUT_FILE_NAME)
+logging.info("Successfully finished cloud identification pre-processing")
+logging.info(f"File written to {OUTPUT_DIR / OUTPUT_FILE_NAME}")
