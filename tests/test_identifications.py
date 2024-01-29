8bdae39fc35dd19e6ebce0a8e125b7237c7908ff
nilsnevertree <49751662+nilsnevertree@users.noreply.github.com>
2024-01-29 16:32:04 +0100

    add tests for new IC and DS match function

--- tests/test_identifications.py
+++ tests/test_identifications.py
@@ -256,3 +256,229 @@ def test_match_clouds_and_cloudcomposite():
     assert isinstance(result, xr.Dataset)
     assert "cloud_composite" in result.data_vars
     xr.testing.assert_identical(result, expected_result)
+
+
+def ds_clouds():
+    """Creates cloud dataset."""
+    return xr.Dataset(
+        {
+            "cloud_id": (("time",), [0, 1]),
+            "start": (("time",), pd.date_range("2020-01-01 3:00", periods=2, freq="D")),
+            "end": (("time",), pd.date_range("2020-01-01 9:00", periods=2, freq="D")),
+        },
+        coords={
+            "time": pd.date_range("2020-01-01 6:00", periods=2, freq="D"),
+        },
+    )
+
+
+def ds_sonde():
+    """Create a dropsonde dataset."""
+    return xr.Dataset(
+        {
+            "temp": (("time",), np.arange(8)),
+        },
+        coords={
+            "time": pd.date_range("2020-01-01 0:00", periods=8, freq="5H"),
+        },
+    )
+
+
+def ds_distance():
+    """Create a distance dataset."""
+    # temporal_distance = ds_cloud.time.rename({"time": "time_identified_clouds"}) - ds_sonde.time.rename({"time": "time_drop_sondes"})
+    # print(temporal_distance.data.astype("timedelta64[h]"))
+    return xr.Dataset(
+        {
+            # spatial distance in km
+            "spatial_distance": (
+                ("time_identified_clouds", "time_drop_sondes"),
+                np.array(
+                    [
+                        [1, 1, 3, 3, 5, 3, 1, 8],
+                        [1, 1, 3, 3, 5, 3, 1, 8],
+                    ],
+                    dtype="int",
+                ),
+            ),
+            # temporal distance in hours
+            "temporal_distance": (
+                ("time_identified_clouds", "time_drop_sondes"),
+                np.array(
+                    [[6, 1, -4, -9, -14, -19, -24, -29], [30, 25, 20, 15, 10, 5, 0, -5]],
+                    dtype="timedelta64[h]",
+                ).astype("timedelta64[ns]"),
+            ),
+        },
+        coords={
+            "time_drop_sondes": pd.date_range("2020-01-01 0:00", periods=8, freq="5H"),
+            "time_identified_clouds": pd.date_range("2020-01-01 6:00", periods=2, freq="D"),
+        },
+    )
+
+
+def selected_dropsondes_5h_3km():
+    """
+    Should result for
+    max_temporal_distance = 5 hours
+    max_spatial_distance = 3 km
+    """
+    return xr.Dataset(
+        {
+            "temp": (("time",), [1, 2, 5, 6]),
+        },
+        coords={
+            "time": [
+                pd.Timestamp("2020-01-01 05:00"),
+                pd.Timestamp("2020-01-01 10:00"),
+                pd.Timestamp("2020-01-02 01:00"),
+                pd.Timestamp("2020-01-02 06:00"),
+            ],
+        },
+    )
+
+
+def selected_dropsondes_1h_10km():
+    """
+    Should result for
+    max_temporal_distance = 5 hours
+    max_spatial_distance = 3 km
+    """
+    return xr.Dataset(
+        {
+            "temp": (("time",), [1, 6]),
+        },
+        coords={
+            "time": [
+                pd.Timestamp("2020-01-01 05:00"),
+                pd.Timestamp("2020-01-02 06:00"),
+            ],
+        },
+    )
+
+
+def selected_dropsondes_0h_0km():
+    """
+    Should result for
+    max_temporal_distance = 0 hours
+    max_spatial_distance = 0 km
+    """
+    return xr.Dataset(
+        {
+            "temp": (("time",), []),
+        },
+        coords={
+            "time": np.array([], dtype="datetime64[ns]"),
+        },
+    )
+
+
+@pytest.mark.parametrize(
+    "max_temporal_distance, max_spatial_distance, expected",
+    [
+        (5, 3, selected_dropsondes_5h_3km()),
+        (1, 10, selected_dropsondes_1h_10km()),
+        (0, 0, selected_dropsondes_0h_0km()),
+    ],
+)
+def test_match_clouds_and_dropsondes(max_temporal_distance, max_spatial_distance, expected):
+    """
+    Tests the match_clouds_and_dropsondes function. It uses a cloud dataset and
+    a dropsonde dataset.
+
+    Example visualisation
+        >>> # Example setup
+        ... # For a max dt = 5 hours
+        ... # For a max dh = 3 km
+        ... # The datasets below can be summarized visually as follows:
+        ... # ->    2020-01-01      <-|-> 2020-01-02
+        ... # ______________________________________
+        ... # 0    5    10   15   20  |1    6    11   # Time in hours
+        ... # ______________________________________
+        ... # ---S--M--E--------------|--S--M--E----  # Cloud start, middle, end time
+        ... # D----D----D----D----D---|D----D----D--  # Dropsonde
+        ... # 1----1----3----3----5---|3----1----8--  # Distance dropsonde to clouds in km (same for both clouds)
+        ... # ______________________________________
+        ... # F----T----T----F----F---|T----T----F--  # Dropsondes close to cloud T for true F for false
+        ... # F----T----T----F----F---|F----F----F--  # Dropsondes close to cloud 0
+        ... # F----T----T----F----F---|T----T----F--  # Dropsondes close to cloud 1
+    """
+    max_temporal_distance = np.timedelta64(max_temporal_distance, "h")
+    result = match_clouds_and_dropsondes(
+        ds_clouds=ds_clouds(),
+        ds_sonde=ds_sonde(),
+        ds_distance=ds_distance(),
+        dim_in_dropsondes="time",
+        dim_in_clouds="time",
+        index_ds_dropsonde="time_drop_sondes",
+        index_ds_clouds="time_identified_clouds",
+        max_temporal_distance=max_temporal_distance,
+        max_spatial_distance=max_spatial_distance,
+        dask_compute=False,
+    )
+    assert isinstance(result, xr.Dataset)
+    xr.testing.assert_equal(result, expected)
+
+
+def test_match_clouds_and_dropsondes_fails():
+    """Checks for correct fails with ValueError is not all values from ds_cloud
+    are in ds_distance."""
+
+    # Test 1:
+    # Fails if values from ds_clouds arent in ds_distance
+
+    ds_distance_subset = ds_distance().isel({"time_identified_clouds": 0})
+    ds_clouds_subset = ds_clouds().isel({"time": 0})
+    ds_sonde_subset = ds_sonde().isel({"time": 0})
+    with pytest.raises(ValueError):
+        match_clouds_and_dropsondes(
+            ds_clouds=ds_clouds(),
+            ds_sonde=ds_sonde(),
+            ds_distance=ds_distance_subset,
+            dim_in_dropsondes="time",
+            dim_in_clouds="time",
+            index_ds_dropsonde="time_drop_sondes",
+            index_ds_clouds="time_identified_clouds",
+            max_temporal_distance=np.timedelta64(5, "h"),
+            max_spatial_distance=3,
+        )
+    # Test 2:
+    # Does not fail if values from ds_clouds arent in ds_distance
+    match_clouds_and_dropsondes(
+        ds_clouds=ds_clouds_subset,
+        ds_sonde=ds_sonde(),
+        ds_distance=ds_distance(),
+        dim_in_dropsondes="time",
+        dim_in_clouds="time",
+        index_ds_dropsonde="time_drop_sondes",
+        index_ds_clouds="time_identified_clouds",
+        max_temporal_distance=np.timedelta64(5, "h"),
+        max_spatial_distance=3,
+    )
+    # Test 3:
+    # Fails if values from ds_sonde arent in ds_distance and vice versa
+    with pytest.raises(ValueError):
+        match_clouds_and_dropsondes(
+            ds_clouds=ds_clouds(),
+            ds_sonde=ds_sonde(),
+            ds_distance=ds_distance_subset,
+            dim_in_dropsondes="time",
+            dim_in_clouds="time",
+            index_ds_dropsonde="time_drop_sondes",
+            index_ds_clouds="time_identified_clouds",
+            max_temporal_distance=np.timedelta64(5, "h"),
+            max_spatial_distance=3,
+        )
+    # Test 4:
+    with pytest.raises(ValueError):
+        match_clouds_and_dropsondes(
+            ds_clouds=ds_clouds(),
+            ds_sonde=ds_sonde_subset,
+            ds_distance=ds_distance(),
+            dim_in_dropsondes="time",
+            dim_in_clouds="time",
+            index_ds_dropsonde="time_drop_sondes",
+            index_ds_clouds="time_identified_clouds",
+            max_temporal_distance=np.timedelta64(5, "h"),
+            max_spatial_distance=3,
+        )
