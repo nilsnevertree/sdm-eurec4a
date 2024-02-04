cac6702a13a1ceeda4e5eb1467311dcc982c9141
nilsnevertree <nils.niebaum@gmx.de>
2024-02-04 21:16:51 +0100

    Update templates_path in conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -18,4 +18,4 @@ html_theme = "furo"
 autosummary_generate = True  # Turn on sphinx.ext.autosummary
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ['templates']
