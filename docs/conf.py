a2075fef888e0d573acf131bfe7fb32d4ad1864c
nilsnevertree <nils.niebaum@gmx.de>
2024-02-04 21:36:27 +0100

    Update sys.path in conf.py to fix source code directory

--- docs/conf.py
+++ docs/conf.py
@@ -2,12 +2,16 @@
 from __future__ import annotations
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../..'))  # Source code dir relative to this file
+sys.path.insert(0, os.path.abspath('..'))  # Source code dir relative to this file
 
 
 project = "Using Super-Droplet Model and EUREC4A data to simulate rain evaporation"
 author = "Nils Niebaum"
 copyright = "2023, Nils Niebaum"
+# Add your repository URL
+github_url = (
+    f"https://github.com/nilsnevertree/kalman-reconstruction-partially-observed-systems"
+)
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.napoleon",
