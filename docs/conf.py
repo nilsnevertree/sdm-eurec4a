d3935d5f30909fa18e4414a936c84ec8f9b7095c
Nils Niebaum <49751662+nilsnevertree@users.noreply.github.com>
2024-02-04 20:11:14 +0000

    Update conf.py
    Use autosummary for sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -8,8 +8,14 @@ copyright = "2023, Nils Niebaum"
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.napoleon",
+    "sphinx.ext.autosummary",  # Create neat summary tables
     "sphinx_click",
     "myst_parser",
 ]
 autodoc_typehints = "description"
 html_theme = "furo"
+
+autosummary_generate = True  # Turn on sphinx.ext.autosummary
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
