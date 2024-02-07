a2836e31a7dcec27bb1d67f5e4d9273d509839ee
nilsnevertree <49751662+nilsnevertree@users.noreply.github.com>
2024-02-07 17:53:09 +0100

    Refactor RepositoryPath class to improve readability and maintainability

--- src/sdm_eurec4a/__init__.py
+++ src/sdm_eurec4a/__init__.py
@@ -4,12 +4,14 @@ from __future__ import annotations
 from pathlib import Path
 from typing import Any
 
-class RepositoryPath():
+
+class RepositoryPath:
     """Path to the repository root."""
+
     def __init__(self, development_regime, *args, **kwargs):
-        if development_regime == 'levante':
-            self.repo_dir = Path('/home/m/m301096/repositories/sdm-eurec4a/')
-            self.data_dir = Path('/work/mh1126/m301096/softlinks/sdm-eurec4a/data')
+        if development_regime == "levante":
+            self.repo_dir = Path("/home/m/m301096/repositories/sdm-eurec4a/")
+            self.data_dir = Path("/work/mh1126/m301096/softlinks/sdm-eurec4a/data")
 
     def set_repo_dir(self, repo_dir):
         self.repo_dir = repo_dir
@@ -19,13 +21,12 @@ class RepositoryPath():
 
     def get_repo_dir(self):
         return self.repo_dir
-    
+
     def get_data_dir(self):
         return self.data_dir
-    
+
     def __call__(self) -> Path:
         return self.repo_dir
-    
+
     def __str__(self) -> str:
         return str(self.repo_dir)
-    
