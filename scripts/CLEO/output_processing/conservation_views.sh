b4d960951a1e6af3642b3ed5ffbd56fc519c43c4
nilsnevertree <49751662+nilsnevertree@users.noreply.github.com>
2025-01-06 15:23:34 +0100

    refactor(output): seperate eulerian and conservation dataset creation

--- scripts/CLEO/output_processing/conservation_views.sh
+++ scripts/CLEO/output_processing/conservation_views.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --job-name=e1d_eulerian_master
 #SBATCH --partition=compute
-#SBATCH --mem=120G
-#SBATCH --time=00:10:00
+#SBATCH --mem=55G
+#SBATCH --time=00:05:00
 #SBATCH --mail-user=nils-ole.niebaumy@mpimet.mpg.de
 #SBATCH --mail-type=FAIL
 #SBATCH --account=mh1126
@@ -40,18 +40,11 @@ microphysics="null_microphysics"
 path2CLEO=${HOME}/CLEO/
 path2sdm_eurec4a=${HOME}/repositories/sdm-eurec4a
 
-eulerian_view=true
-concatenate_eulerian_view=true
+inflow_outflow=true
+concatenate_inflow_outflow=true
 
-# inflow_outflow=true
-# concatenate_inflow_outflow=true
-
-
-eulerian_view_pythonscript=${path2sdm_eurec4a}/scripts/CLEO/output_processing/create_eulerian_views_mpi4py.py
-concatenate_ev_pythonscript=${path2sdm_eurec4a}/scripts/CLEO/output_processing/concatenate_eulerian_views.py
-
-# inflow_outflow_pyhtonscript=${path2sdm_eurec4a}/scripts/CLEO/output_processing/create_inflow_outflow_mpi4py.py
-# concatenate_io_pythonscript=${path2sdm_eurec4a}/scripts/CLEO/output_processing/concatenate_inflow_outflow.py
+inflow_outflow_pyhtonscript=${path2sdm_eurec4a}/scripts/CLEO/output_processing/create_inflow_outflow_mpi4py.py
+concatenate_io_pythonscript=${path2sdm_eurec4a}/scripts/CLEO/output_processing/concatenate_inflow_outflow.py
 
 path2data=${path2CLEO}/data/output_v4.0/${microphysics}/
 
@@ -62,10 +55,10 @@ echo "microphysics: ${microphysics}"
 if [ ! -d "$path2data" ]; then
     echo "Invalid path to data"
     exit 1
-elif [ ! -f "$eulerian_view_pythonscript" ]; then
+elif [ ! -f "$inflow_outflow_pyhtonscript" ]; then
     echo "Python script not found: ${eulerian_view_pythonscript}"
     exit 1
-elif [ ! -f "$concatenate_ev_pythonscript" ]; then
+elif [ ! -f "$concatenate_io_pythonscript" ]; then
     echo "Python script not found: ${concatenate_ev_pythonscript}"
     exit 1
 else
@@ -73,32 +66,17 @@ else
 fi
 echo "============================================"
 
-if [ "$create" = true ]; then
-    echo "Create eulerian views"
-    mpirun -np 20 python ${eulerian_view_pythonscript} --data_dir ${path2data}
+if [ "$inflow_outflow" = true ]; then
+    echo "Create Inflow Outflow"
+    # python ${inflow_outflow_pyhtonscript} --data_dir ${path2data}
+    mpirun -np 20 python ${inflow_outflow_pyhtonscript} --data_dir ${path2data}
     wait
     echo "============================================"
 fi
 
-if [ "$concatenate_eulerian_view" = true ]; then
-    echo "Concatenate Eulerian Views with dependency of create eulerian views"
-    python ${concatenate_ev_pythonscript} --data_dir ${path2data}
+if [ "$concatenate_inflow_outflow" = true ]; then
+    echo "Concatenate Inflow Outflow datasets"
+    python ${concatenate_io_pythonscript} --data_dir ${path2data}
     echo "============================================"
 fi
-
-# if [ "$inflow_outflow" = true ]; then
-#     echo "Create Inflow Outflow"
-#     # python ${inflow_outflow_pyhtonscript} --data_dir ${path2data}
-#     mpirun -np 20 python ${inflow_outflow_pyhtonscript} --data_dir ${path2data}
-#     wait
-#     echo "============================================"
-# fi
-
-# if [ "$concatenate_inflow_outflow" = true ]; then
-#     echo "Concatenate Inflow Outflow datasets"
-#     python ${concatenate_io_pythonscript} --data_dir ${path2data}
-#     echo "============================================"
-# fi
-
-
 echo "============================================"
