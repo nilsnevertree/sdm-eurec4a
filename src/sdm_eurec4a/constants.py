4d588658ceb26b72c38a4bc5a231f1b35c04f83c
nilsnevertree <49751662+nilsnevertree@users.noreply.github.com>
2025-03-06 13:34:12 +0100

    feat(constants): #143 Constants module needed for consistent calculation of variables

---
+++ src/sdm_eurec4a/constants.py
@@ -0,0 +1,10 @@
+"""
+Module containing constants used in the project.
+"""
+
+
+class WaterConstants:
+    # density of water
+    density = 1000  # kg / m^3
+    # latent heat of vaporization of water
+    vapourization_heat = 2257e3  # J / kg
